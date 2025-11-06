573f8dc19f8d9b35db82d4e56d417d1615cf3961
Sergew_RMZ <serge15games@gmail.com>
2025-11-05 23:09:23 -0600

    Correcciones realizadas

--- 4. Servicio Web REST/src/app.ts
+++ 4. Servicio Web REST/src/app.ts
@@ -3,13 +3,13 @@ import { AppRoutes } from './routes.js';
 import { envs } from './config/envs.js';
 
 (async () => {
-  main();
-});
+  await main();
+})();
 async function main () {
   const server = new Server({
     port: envs.PORT,
     routes: AppRoutes.routes,
   });
 
-  server.start();
+  await server.start();
 }
--- 4. Servicio Web REST/src/server.ts
+++ 4. Servicio Web REST/src/server.ts
@@ -38,7 +38,7 @@ export class Server {
     this.app.use(this.routes);
 
     //* SPA /^\/(?!api).*/  <== Únicamente si no empieza con la palabra api
-    this.app.get("*", (req, res) => {
+    this.app.get(/.*/, (req, res) => {
       const indexPath = path.join(
         __dirname + `../../../${this.publicPath}/index.html`
       );
