25ee4db9cf7d169d095e5f10d38e0cef25d7d99e
Sergew_RMZ <serge15games@gmail.com>
2025-12-13 18:53:09 -0600

    Progressive Web Application Terminada

--- 5. Progressive Web App/react-pwa/vite.config.js
+++ 5. Progressive Web App/react-pwa/vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      
+      manifest: {
+        name: 'Cloud Storage App',
+        short_name: 'CloudStorage',
+        description: 'Aplicación para subir y gestionar archivos en la nube.',
+        theme_color: '#1976d2',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'nube-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'nube-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
